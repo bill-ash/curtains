8b2a1806ddcf769f88262b05e7af0a5aac4bfa36
billash <wrash0806@my.mwsu.edu>
2020-09-26 02:16:29 -0400

    Go build a flask app.

--- inst/shiny-curtains/server.R
+++ inst/shiny-curtains/server.R
@@ -1,4 +1,25 @@
 #Server
 function(input, output, session) {
 
+  fortunes::fortune()
+
+  # POST TO PI WHEN BUTTONS ARE ENGAGED
+  observeEvent(input$open_curtains, {
+
+    showModal(
+      modalDialog('You opened the curtains!', footer = NULL,
+                  easyClose = TRUE)
+    )
+    # Some stuff
+  })
+
+  observeEvent(input$close_curtains, {
+    # Go write a function
+    showModal(
+      modalDialog('You closed the curtains!', footer = NULL,
+                  easyClose = TRUE)
+    )
+
+  })
+
 }
--- inst/shiny-curtains/ui.R
+++ inst/shiny-curtains/ui.R
@@ -1,4 +1,16 @@
 #UI
 fluidPage(
-  'Hello World!'
+
+  fluidRow(
+    column(4,
+           h3("Open it.. just a bit...."),
+           actionButton('open_curtains', 'Open'),
+           offset = 1
+    ),
+    column(4,
+           h3("Time for bed!"),
+           actionButton('close_curtains', 'Close')
+    )
+  )
+
 )
